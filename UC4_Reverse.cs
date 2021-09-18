fb02bc8cafe57da3d23c3e56699303a80865d9f8
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-18 22:33:23 +0530

    UC4 Reverse program added

---
+++ UC4_Reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_LogicalPrograms
+{
+    class UC4_Reverse
+    {
+        public static void rev()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a number: ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.Write("Reversed Number: " + reverse);
+        }
+
+    }
+}
