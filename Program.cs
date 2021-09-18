a3f18f5decf003c0abc545feeecc4043d87acc77
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-18 22:28:30 +0530

    Main file i.e program .cs added

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day6_LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1_Fibb.fibb();
+             //UC2_Perfect.per();
+            //UC3_Prime.prime();
+            //UC4_Reverse.rev();
+            //UC5_Coupon.cou();
+            //UC6_Stopwatch.stopWatch();
+
+        }
+    }
+}
