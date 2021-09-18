2876e77e53c3a14830719679f44a7e752627e407
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-18 22:31:56 +0530

    UC2 Primenumber program added

---
+++ UC3_Prime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6_LogicalPrograms
+{
+    class UC3_Prime
+    {
+        public static void prime()
+        {
+            Console.Write("Enter a Number : ");
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool IsPrime = true;
+
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    IsPrime = false;
+                    break;
+                }
+            }
+            if (IsPrime)
+            {
+                Console.Write("Number is Prime.");
+            }
+            else
+            {
+                Console.Write("Number is not Prime.");
+            }
+
+
+        }
+
+        }
+}
